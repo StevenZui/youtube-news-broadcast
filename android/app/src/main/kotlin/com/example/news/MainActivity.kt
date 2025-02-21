6ec2bf5515241ada38486a3ec5bfaa2c305d46d9
Steven Zui <stevenpdev@gmail.com>
2025-02-21 18:13:32 +0800

    wip

--- android/app/src/main/kotlin/com/example/news/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/news/MainActivity.kt
@@ -1,5 +1,54 @@
 package com.example.news
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    private var sharedText: String? = null
+    private val CHANNEL = "app.channel.shared.data"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = getIntent()
+        handleIntent(intent)
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        handleIntent(intent)
+    }
+
+
+    private fun handleIntent(intent: Intent) {
+        val action = intent.action
+        val type = intent.type
+
+        if (Intent.ACTION_VIEW == action && type != null) {
+            if ("text/plain" == type) {
+                handleSendText(intent)
+            }
+        }
+    }
+
+    private fun handleSendText(intent: Intent) {
+        sharedText = intent.getStringExtra(Intent.EXTRA_TEXT)
+    }
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+//        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+//            if (call.method ==("handleShortcut")) {
+//                result.success(sharedText)
+//                sharedText = null
+//            }
+//        }
+
+        val shortcutId = intent.getStringExtra("shortcutId")
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL)
+            .invokeMethod("handleShortcut", shortcutId)
+    }
+}
